70027d0b09528bd3019e93bb73e5ed108e522e7d
김도형 <maam10@naver.com>
2023-01-11 17:33:57 +0900

    [MEMBER] 회원가입 작업자 : 김도형 작업내용 : 중복예외처리 , 가입시 프로필정보생성, 유효성

--- server/src/main/java/com/gigker/server/domain/member/repository/MemberRepository.java
+++ server/src/main/java/com/gigker/server/domain/member/repository/MemberRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gigker.server.domain.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long>
+{
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickName(String nickname);
 
 }
