3f4aae0665e6af2a4b7929f417305c43cb18484f
H-JWANNA <hongjjwan@gmail.com>
2023-01-11 14:28:26 +0900

    [#13] feat: 에러 코드 추가

--- server/src/main/java/com/gigker/server/global/exception/ExceptionCode.java
+++ server/src/main/java/com/gigker/server/global/exception/ExceptionCode.java
@@ -10,7 +10,11 @@ public enum ExceptionCode {
 	// 404
 	NOT_FOUND_MEMBER(404, "Member not found"),
 	NOT_FOUND_CONTENT(404, "Content not found"),
-	NOT_FOUND_REVIEW(404, "Review not found");
+	NOT_FOUND_APPLY(404, "Apply not found"),
+	NOT_FOUND_REVIEW(404, "Review not found"),
+
+	// 409
+	APPLY_EXISTS(409, "Applicant already applied");
 
 	int code;
 	String message;
