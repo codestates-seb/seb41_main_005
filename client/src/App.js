47ded674006fef8a52f67466db75d47e9c8223a4
Haejeong <skhj5822@naver.com>
2023-01-11 13:09:26 +0900

    feat: server 통신 테스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+
 function App() {
+  axios({
+    method: "post",
+    url: "http://gigker.iptime.org:8080/members", // https://3cd6-211-227-190-110.jp.ngrok.io/members",
+    // data: {
+    //   email: "email@email.com",
+    //   nickName: "testUser",
+    //   password: 1234,
+    //   about: "about me!",
+    // },
+  }).then((res) => console.log(res));
+
   return <div>test</div>;
 }
 
