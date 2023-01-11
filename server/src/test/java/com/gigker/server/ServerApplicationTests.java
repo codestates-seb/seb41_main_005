159c16856bf05dd99a0672e3653bccb3ea69439b
H-JWANNA <hongjjwan@gmail.com>
2023-01-11 14:28:08 +0900

    [#13] fix: 빌드 안되는 오류 해결

--- server/src/test/java/com/gigker/server/ServerApplicationTests.java
+++ server/src/test/java/com/gigker/server/ServerApplicationTests.java
@@ -1,9 +1,8 @@
 package com.gigker.server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ServerApplicationTests {
 
 	@Test
