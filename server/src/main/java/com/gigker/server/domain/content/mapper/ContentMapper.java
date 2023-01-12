27dc2632e6fc94427399168b12fd3f065b122b77
harrisonk213 <valntno928@gmail.com>
2023-01-12 13:13:18 +0900

    [CONTENT] Content post 구현완료

--- server/src/main/java/com/gigker/server/domain/content/mapper/ContentMapper.java
+++ server/src/main/java/com/gigker/server/domain/content/mapper/ContentMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring" , unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring" , unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = MemberMaper.class)
 public interface ContentMapper {
 
     @Mapping(source = "memberId", target = "member.memberId")
