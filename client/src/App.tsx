2e521d546531c23b54b8641a591b052b0a27e458
hyob12 <gyqud5981@gmail.com>
2023-01-12 14:57:30 +0900

    GlobalStyle & Theme 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import Navigation from "./components/Navigation";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./assets/styles/global-styles";
+import { theme } from "./assets/styles/theme";
+
 function App() {
-  return <div>test</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Navigation />
+    </ThemeProvider>
+  );
 }
 
 export default App;
