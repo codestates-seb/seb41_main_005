824703450fb61c2d99547c867ef2e25520f37292
harrisonk213 <valntno928@gmail.com>
2023-01-16 04:57:11 +0900

    [CONTENT] Content 삭제 기능 추가

--- server/src/main/java/com/gigker/server/domain/content/mapper/ContentMapper.java
+++ server/src/main/java/com/gigker/server/domain/content/mapper/ContentMapper.java
@@ -9,6 +9,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring" , unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = MemberMapper.class)
 public interface ContentMapper {
 
