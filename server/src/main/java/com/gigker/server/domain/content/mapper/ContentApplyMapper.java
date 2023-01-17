33cff9864c0f52b160340cd9ef648833f9337257
H-JWANNA <hongjjwan@gmail.com>
2023-01-17 14:50:28 +0900

    [#44] feat: 지원자 정보를 출력하기 위한 Dto, Mapper 수정

--- server/src/main/java/com/gigker/server/domain/content/mapper/ContentApplyMapper.java
+++ server/src/main/java/com/gigker/server/domain/content/mapper/ContentApplyMapper.java
@@ -6,25 +6,33 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-import com.gigker.server.domain.content.dto.ContentApplyDto;
+import com.gigker.server.domain.content.dto.ContentApplyResponseDto;
+import com.gigker.server.domain.content.entity.Content;
 import com.gigker.server.domain.content.entity.ContentApply;
+import com.gigker.server.domain.member.mapper.MemberMapper;
 
 @Mapper(componentModel = "spring",
-	// uses = {MemberMapper.class, ContentMapper.class},
+	uses = {MemberMapper.class, ContentMapper.class},
 	unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ContentApplyMapper {
 
 	@Mapping(source = "post.applicantId", target = "applicant.memberId")
 	@Mapping(source = "contentId", target = "content.contentId")
-	ContentApply postToApply(ContentApplyDto.Post post, Long contentId);
+	ContentApply postToApply(ContentApplyResponseDto.Post post, Long contentId);
 
+	@Mapping(source = "applicant.memberId", target = "applicantId")
 	@Mapping(source = "applicant.nickName", target = "nickName")
 	@Mapping(source = "applicant.pictureUrl", target = "pictureUrl")
 	@Mapping(source = "applicant.about", target = "about")
-	ContentApplyDto.Response applyToResponse(ContentApply apply);
+	@Mapping(target = "like", expression = "java(apply.getLikeCount())")
+	@Mapping(target = "dislike", expression = "java(apply.getDislikeCount())")
+	ContentApplyResponseDto.Applicant applyToApplicant(ContentApply apply);
 
+	@Mapping(source = "applicant.memberId", target = "applicantId")
 	@Mapping(source = "applicant.nickName", target = "nickName")
 	@Mapping(source = "applicant.pictureUrl", target = "pictureUrl")
 	@Mapping(source = "applicant.about", target = "about")
-	List<ContentApplyDto.Response> appliesToResponses(List<ContentApply> applies);
+	@Mapping(target = "like", expression = "java(apply.getLikeCount())")
+	@Mapping(target = "dislike", expression = "java(apply.getDislikeCount())")
+	List<ContentApplyResponseDto.Applicant> appliesToApplicants(List<ContentApply> applies);
 }
