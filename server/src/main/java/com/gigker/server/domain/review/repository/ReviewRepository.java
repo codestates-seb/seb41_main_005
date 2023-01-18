ce1c0760c4f8a70405f2865ba9876949f4fecc46
H-JWANNA <hongjjwan@gmail.com>
2023-01-18 17:09:23 +0900

    [#62] feat: Repository에 쿼리문 추가

--- server/src/main/java/com/gigker/server/domain/review/repository/ReviewRepository.java
+++ server/src/main/java/com/gigker/server/domain/review/repository/ReviewRepository.java
@@ -3,6 +3,8 @@ package com.gigker.server.domain.review.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gigker.server.domain.content.entity.Content;
 import com.gigker.server.domain.member.entity.Member;
@@ -10,5 +12,7 @@ import com.gigker.server.domain.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-	Optional<Review> findByWriterAndContent(Member writer, Content content);
+	@Query("select rv from Review rv " +
+		"where rv.writer.applicant = :writer and rv.writer.content = :content")
+	Optional<Review> findByWriterAndContent(@Param("writer") Member member, @Param("content") Content content);
 }
