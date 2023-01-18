e7bb07ca6a91538155d39a6ff7ad0d84818cab37
harrisonk213 <valntno928@gmail.com>
2023-01-18 22:35:02 +0900

    [CONTENTTAG] 주석제거

--- server/src/main/java/com/gigker/server/ServerApplication.java
+++ server/src/main/java/com/gigker/server/ServerApplication.java
@@ -2,12 +2,7 @@ package com.gigker.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -15,27 +10,5 @@ public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*");
-                corsFilter();
-            }
-
-            public void corsFilter() {
-                CorsConfiguration corsConfiguration = new CorsConfiguration();
-                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                corsConfiguration.addAllowedOriginPattern("*");
-                corsConfiguration.addAllowedMethod("*");
-                corsConfiguration.addAllowedHeader("*");
-                corsConfiguration.addExposedHeader("authorization");
-                source.registerCorsConfiguration("/**", corsConfiguration);
-            }
-        };
     }
 }
