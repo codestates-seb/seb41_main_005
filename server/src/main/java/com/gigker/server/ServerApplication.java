87ff126ee552d8c13c7b7a86a028b32dd9d0b411
harrisonk213 <111319814+harrisonk213@users.noreply.github.com>
2023-01-19 11:20:07 +0900

    Update ServerApplication.java

--- server/src/main/java/com/gigker/server/ServerApplication.java
+++ server/src/main/java/com/gigker/server/ServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
