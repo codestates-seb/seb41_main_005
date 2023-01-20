7597db3b788e5627d6cefa1be1f8cc3eca2aec69
Haejeong <skhj5822@naver.com>
2023-01-20 16:19:51 +0900

    pull dev branch

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,3 +1,51 @@
-export default function Main() {
-  return <div>main</div>;
+import styled from "styled-components";
+import axios from "axios";
+import Footer from "../components/Footer";
+
+const Container = styled.div`
+  display: block;
+  max-width: 1060px;
+  padding-top: 70px;
+  .section {
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const StyledArticle = styled.article`
+  display: block;
+  position: relative;
+  background-image: none;
+`;
+
+function Main() {
+  return (
+    <Container>
+      <section>
+        <article>
+          <header>
+            <div>
+              <p>구인</p>
+            </div>
+            <div>
+              <button>왼버튼</button>
+              <button>오버튼</button>
+            </div>
+          </header>
+          <p>1</p>
+        </article>
+        <article>
+          <header>구인</header>
+          <p>2</p>
+        </article>
+        <StyledArticle>
+          <header>구직</header>
+          <p>3</p>
+        </StyledArticle>
+      </section>
+      <Footer />
+    </Container>
+  );
 }
+
+export default Main;
