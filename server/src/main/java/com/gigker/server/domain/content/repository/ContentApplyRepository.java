486251d40bef2baedb6c1487d39b33a0e9469bd1
H-JWANNA <hongjjwan@gmail.com>
2023-01-27 19:31:27 +0900

    [#120] feat: 완료한 건 수 조회하는 쿼리 추가

--- server/src/main/java/com/gigker/server/domain/content/repository/ContentApplyRepository.java
+++ server/src/main/java/com/gigker/server/domain/content/repository/ContentApplyRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.gigker.server.domain.common.ContentType;
 import com.gigker.server.domain.content.entity.Content;
 import com.gigker.server.domain.content.entity.ContentApply;
 import com.gigker.server.domain.member.entity.Member;
@@ -26,17 +27,29 @@ public interface ContentApplyRepository extends JpaRepository<ContentApply, Long
 	// ContentApply 상태에 따라서 조회
 	List<ContentApply> findAllByApplyStatus(ContentApply.ApplyStatus applyStatus);
 
-	// ===== Schedule =====
+	// ===== MATCH Schedule =====
 
 	// 내가 신청한 스케쥴 목록 확인
 	@Query("select DISTINCT ca from ContentApply ca " +
-			"join fetch ca.applicant m " +
-			"where m = :applicant and ca.applyStatus = 'MATCH'")
+		"join fetch ca.applicant m " +
+		"where m = :applicant and ca.applyStatus = 'MATCH'")
 	List<ContentApply> findAllByMatchedApplicant(@Param("applicant") Member applicant);
 
 	// 내가 작성한 스케쥴 목록 확인
 	@Query("select DISTINCT ca from ContentApply ca " +
-			"join fetch ca.content c join fetch c.member " +
-			"where ca.applyStatus = 'MATCH'")
+		"join fetch ca.content c join fetch c.member m " +
+		"where m = :writer and ca.applyStatus = 'MATCH'")
 	List<ContentApply> findAllByMatchedWriter(@Param("writer") Member writer);
+
+	// ===== COMPLETE =====
+
+	// 종합 완료 수
+	@Query("select count (ca) from ContentApply ca " +
+		"where ca.applicant = :applicant and ca.applyStatus = 'COMPLETE'")
+	Long countByApplicant(@Param("applicant") Member applicant);
+
+	// 게시글 타입별 완료 수
+	@Query("select count (ca) from ContentApply ca " +
+		"where ca.applicant = :applicant and ca.content.contentType = :type and ca.applyStatus = 'COMPLETE'")
+	Long countByApplicantAndContentType(@Param("applicant") Member applicant, @Param("type") ContentType type);
 }
