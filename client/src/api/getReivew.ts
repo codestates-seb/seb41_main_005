b09b1f051526a18faf4e8583cac504f5a03703ec
Haejeong <skhj5822@naver.com>
2023-01-29 02:46:49 +0900

    design: css 수정(divider 추가, background-color 추가)

--- client/src/api/getReivew.ts
+++ client/src/api/getReivew.ts
@@ -5,7 +5,6 @@ export const getReview = async (contentId: number) => {
     const response = await axios.get(
       `http://ec2-3-39-239-42.ap-northeast-2.compute.amazonaws.com:8080/reviews/contents/${contentId}?page=1`
     );
-    console.log(response.data);
     return response.data;
   } catch (err) {
     console.log("Error: ", err);
