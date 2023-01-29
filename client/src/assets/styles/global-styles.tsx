99af8573a197d69e6dc16032c992906e38e5b035
hyob12 <gyqud5981@gmail.com>
2023-01-29 17:31:05 +0900

    style/ global-styles 수정

--- client/src/assets/styles/global-styles.tsx
+++ client/src/assets/styles/global-styles.tsx
@@ -10,11 +10,8 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: 'Noto Sans KR', sans-serif;
-<<<<<<< HEAD
-=======
     overflow-y: scroll;
     line-height: 1.4;
->>>>>>> 63201111f2ab941dcb3284b8aa360e73701121ca
   }
   a {
     cursor: pointer;
