a3199e8fe69047d04ca74f4b6c633ff4bee147b6
Hong Jeongwan <hongjjwan@gmail.com>
2023-04-19 17:37:14 +0900

    [#157] refact: 서비스 로직 분리 및 더티 체킹 개선 (#158)

--- server/src/main/java/com/gigker/server/domain/content/repository/ContentRepository.java
+++ server/src/main/java/com/gigker/server/domain/content/repository/ContentRepository.java
@@ -3,6 +3,10 @@ package com.gigker.server.domain.content.repository;
 import com.gigker.server.domain.category.entity.Category;
 import com.gigker.server.domain.common.ContentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gigker.server.domain.content.entity.Content;
 
@@ -12,9 +16,17 @@ public interface ContentRepository extends JpaRepository<Content, Long> {
 
     List<Content> findContentsByContentType(ContentType contentType);
 
-    List<Content> findAllByStatus(Content.Status status);
 
     List<Content> findAllByStatusAndContentType(Content.Status status, ContentType type);
 
     List<Content> findContentsByCategory(Category category);
+
+    // == DeadLine Management ==
+    @Query("select c from Content c where c.status = :status")
+    List<Content> findAllByStatus(@Param("status") Content.Status status);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Content c set c.status = 'RECRUITING' where c.contentId in :ids")
+    void updateContentStatusByIds(@Param("ids") List<Long> contentIds);
 }
